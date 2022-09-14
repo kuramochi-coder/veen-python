4eb0d87c41a7a8e7a089ae659b43c29fe824933b
raphaelkw <raphael.kw@gmail.com>
2022-09-14 13:01:44 +0800

    code clean up

--- NSGGeneratorV1.py
+++ NSGGeneratorV1.py
@@ -5,26 +5,26 @@ from sys import argv
 
 
 # Class below takes in an object, it then removes the quotes and colons to prepare for tfvars syntax
-class DictWithoutQuotedKeys(dict):
+class TerraformObjWithoutQuotedKeys(dict):
     def __repr__(self):
-        # Strip quotes and colons from keys and append to string s
-        s = "\n{"
+        # Strip quotes and colons from keys and append to variable, formatted_object
+        formatted_object = "\n{"
         for key in self:
-            s += "{0}=".format(key)
+            formatted_object += "{0}=".format(key)
 
             if isinstance(self[key], dict):
                 # Apply formatting recursively
-                s += "{0}, ".format(DictWithoutQuotedKeys(self[key]))
+                formatted_object += "{0}, ".format(TerraformObjWithoutQuotedKeys(self[key]))
             else:
                 # Quote all the values
-                s += "\"{0}\"\n ".format(self[key])
-        if len(s) > 1:
-            s = s[0: -2]
-        s += "}\n"
-        return s
+                formatted_object += "\"{0}\"\n ".format(self[key])
+        if len(formatted_object) > 1:
+            formatted_object = formatted_object[0: -2]
+        formatted_object += "}\n"
+        return formatted_object
 
 # Function below reads a csv file with Headers and converts to json file.
-def nsg_generator(terraform_variable_name):
+def nsg_generator(var_name):
     # Get all csv files within the current directory.
     path = "./*.csv"
     files = glob.glob(path)
@@ -41,12 +41,12 @@ def nsg_generator(terraform_variable_name):
         # Prepare the formatting requited, which is an array of objects for the terraform variable 
         final_output_array = []
         for single_obj in dict_output:
-            tfvars_obj = DictWithoutQuotedKeys(single_obj)
+            tfvars_obj = TerraformObjWithoutQuotedKeys(single_obj)
             final_output_array.append(tfvars_obj)
         
         # Create a tfvars file and write the object 
         with open(tfvars_output_filename, 'w') as file:
-            file.write(f'{terraform_variable_name} = {final_output_array}')
+            file.write(f'{var_name} = {final_output_array}')
 
 if __name__ == "__main__":
     variable_name = argv[1]
